1b9b0b05fcff6094f6fc90995d655b1bed3bce4a
abhijeet02323 <dwivedi.abhijeet1301@gmail.com>
2025-09-20 18:29:49 +0530

    read file by using, with open(filepath)

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath, 'r') as book:
+        return  book.read()
+
+def main():
+    book_content = get_book_text("books/frankenstein.txt")
+    
+    print(book_content)
+
+main()
